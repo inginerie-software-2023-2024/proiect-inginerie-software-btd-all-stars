b8b769ab83edf0be2d9976cca7657879055928d8
Andrei Tava <tavaandrei@gmail.com>
2023-11-25 22:26:45 +0200

    save cleared rooms

--- Assets/Scripts/ScriptableObjects/SaveManager.cs
+++ Assets/Scripts/ScriptableObjects/SaveManager.cs
@@ -12,8 +12,9 @@ public class SaveManager : ScriptableObject
     [Serializable]
     class GameState
     {
-        public Vector2 PlayerPosition;
+        public Vector2 playerPosition;
         public List<string> upgrades;
+        public List<int> clearedRooms;
     }
 
     private GameState _state;
@@ -25,11 +26,25 @@ public class SaveManager : ScriptableObject
     public void Save()
     {
         var player = GameObject.FindGameObjectsWithTag("Player").FirstOrDefault();
-        _state.PlayerPosition = player.transform.position;
+        _state.playerPosition = player.transform.position;
+        
+        _state.upgrades.Clear();
         foreach(var upgrade in _inventory.upgrades)
         {
             _state.upgrades.Add(upgrade.GetType().Name);
         }
+        var rooms = FindObjectsByType<EnemyHandler>(FindObjectsSortMode.InstanceID);
+        _state.clearedRooms.Clear();
+        for(int i = 0; i < rooms.Length; i++)
+        {
+            
+            if (rooms[i].RoomCleared)
+            {
+                Debug.Log(rooms[i].name);
+                _state.clearedRooms.Add(i);
+            }
+        }
+        Debug.Log(_state.clearedRooms.Count);
 
         WriteToSaveFile();
 
