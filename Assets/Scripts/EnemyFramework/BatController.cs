963a38d5cb271dfdd4a9dc96189c068654a35b50
Roșianu Robert <rosianu.robert@yahoo.com>
2023-12-17 23:14:12 +0200

    Add enemy 3

---
+++ Assets/Scripts/EnemyFramework/BatController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace EnemyFramework
+{
+    public class BatController : GenericEnemyController
+    {
+        [SerializeField]
+        private float _alertRadius = 0.2f;
+
+        [SerializeField]
+        private Vector2 _movementDirection;
+        protected new void Start()
+        {
+            base.Start();
+            attackingDirection = Vector2.Perpendicular(_movementDirection).normalized;
+            target = GameObject.FindWithTag("Player").transform;
+        }
+        protected override void AttackSequence()
+        {
+            currentState = EnemyState.attacking;
+            StartCoroutine(AttackRoutine());
+        }
+
+        protected override bool ConditionIsSatisfied()
+        {
+
+            return (currentState == EnemyState.moving || currentState == EnemyState.idle) 
+                   && (target.position - transform.position).magnitude > _alertRadius ;
+
+        }
+        protected override void IdleBehaviour()
+        {
+            if (currentState == EnemyState.idle || currentState == EnemyState.moving)
+            {
+                movementDirection = (target.transform.position - transform.position);
+
+                if (movementDirection.magnitude > _alertRadius)
+                    return;
+
+                movementDirection = movementDirection.normalized;
+                currentState = EnemyState.moving;
+                enemyRigidbody.MovePosition(transform.position + speed * Time.deltaTime * movementDirection);
+
+            }
+        }
+
+        protected IEnumerator AttackRoutine()
+        {
+            movementDirection = target.transform.position - transform.position;
+            yield return new WaitForSeconds(0.001f);
+            movementDirection = movementDirection.normalized;
+            currentState = EnemyState.attacking;
+            enemyRigidbody.MovePosition(transform.position + speed * Time.deltaTime * movementDirection);
+        }
+    }
+}
